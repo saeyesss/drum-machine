9c6f5ba5a270c039d1238c34cbba89e2b1da31b6
saeyesss <94211639+saeyesss@users.noreply.github.com>
2022-06-12 18:05:20 +0530

    play samples basic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from pygame import mixer
+
 pygame.init()
 
 # window dimension
@@ -9,26 +10,55 @@ HEIGHT = 800
 black = (0, 0, 0)
 white = (255, 255, 255)
 gray = (128, 128, 128)
-green = (0,255, 0)
+green = (0, 255, 0)
 gold = (212, 175, 55)
+blue = (0, 255, 255)
 
 screen = pygame.display.set_mode([WIDTH, HEIGHT])
 pygame.display.set_caption("Drum-Machine")
-label_font = pygame.font.Font('Roboto-Bold.ttf', 32)
+label_font = pygame.font.Font('./public/Roboto-Bold.ttf', 32)
 
 fps = 60
 timer = pygame.time.Clock()
 beats = 16
 instruments = 6
 boxes = []
-clicked = [[-1 for  _ in range(beats)] for _ in range(instruments)] # -1 is for those that are not selected
+clicked = [[-1 for _ in range(beats)] for _ in range(instruments)]  # -1 is for those that are not selected
 bpm = 240
+playing = True
+active_length = 0
+active_beat = 0
+beat_changed = True
+
+# load drum samples
+hi_hat = mixer.Sound('.\public\sounds\hi hat.wav')
+snare = mixer.Sound('.\public\sounds\snare.wav')
+kick = mixer.Sound('.\public\sounds\kick.wav')
+crash = mixer.Sound('.\public\sounds\crash.wav')
+clap = mixer.Sound('.\public\sounds\clap.wav')
+tom = mixer.Sound(".\public\sounds\\tom.wav")
 
 
-def draw_grid(clicks):
+def play_notes():
+    for i in range(len(clicked)):
+        if clicked[i][active_beat] == 1:
+            if i == 0:
+                hi_hat.play()
+            if i == 1:
+                snare.play()
+            if i == 2:
+                kick.play()
+            if i == 3:
+                crash.play()
+            if i == 4:
+                clap.play()
+            if i == 5:
+                tom.play()
+                
 
-    left_box = pygame.draw.rect(screen, gray, [0, 0, 200, HEIGHT-200], 5)
-    bottom_box = pygame.draw.rect(screen, gray, [0, HEIGHT-200, WIDTH, 200], 5)
+def draw_grid(clicks, beat):
+    left_box = pygame.draw.rect(screen, gray, [0, 0, 200, HEIGHT - 200], 5)
+    bottom_box = pygame.draw.rect(screen, gray, [0, HEIGHT - 200, WIDTH, 200], 5)
 
     boxes = []
     colors = [gray, white, gray]
@@ -46,11 +76,11 @@ def draw_grid(clicks):
     tom_text = label_font.render('Tom', True, white)
     screen.blit(tom_text, (30, 530))
 
-# outer lines per instrument
+    # outer lines per instrument
     for i in range(instruments):
-        pygame.draw.line(screen, gray, (0, (i*100)+100), (200, (i*100)+100), 3)
+        pygame.draw.line(screen, gray, (0, (i * 100) + 100), (200, (i * 100) + 100), 3)
 
-# grid for the sequencer
+    # grid for the sequencer
     for i in range(beats):
         for j in range(instruments):
             if clicks[j][i] == -1:
@@ -58,18 +88,20 @@ def draw_grid(clicks):
             else:
                 color = green
 
-            rect = pygame.draw.rect(screen, color, [i * ((WIDTH-200) // beats) + 205, (j*100)+5,
-                                                   ((WIDTH-200)//beats)-10, ((HEIGHT-200) // instruments)-10], 0, 3)
+            rect = pygame.draw.rect(screen, color, [i * ((WIDTH - 200) // beats) + 205, (j * 100) + 5,
+                                                    ((WIDTH - 200) // beats) - 10,
+                                                    ((HEIGHT - 200) // instruments) - 10], 0, 3)
 
             pygame.draw.rect(screen, gold, [i * ((WIDTH - 200) // beats) + 200, (j * 100),
-                                             ((WIDTH - 200) // beats), ((HEIGHT - 200) // instruments)], 5, 10)
+                                            ((WIDTH - 200) // beats), ((HEIGHT - 200) // instruments)], 5, 10)
 
             pygame.draw.rect(screen, black, [i * ((WIDTH - 200) // beats) + 200, (j * 100),
-                                                   ((WIDTH - 200) // beats), ((HEIGHT - 200) // instruments)], 2, 10)
-            boxes.append((rect,(i,j)))
-
+                                             ((WIDTH - 200) // beats), ((HEIGHT - 200) // instruments)], 2, 10)
+            boxes.append((rect, (i, j)))
+        active = pygame.draw.rect(screen, blue,
+                                  [beat * ((WIDTH - 200) // beats) + 200, 0,
+                                   ((WIDTH - 200) // beats), instruments * 100], 5, 3)
     return boxes
-
 
 
 # main event loop
@@ -77,7 +109,11 @@ run = True
 while run:
     timer.tick(fps)
     screen.fill(black)
-    boxes = draw_grid(clicked)
+    boxes = draw_grid(clicked, active_beat)
+
+    if beat_changed:
+        play_notes()
+        beat_changed = False
 
     # event handling
     for event in pygame.event.get():
@@ -90,10 +126,19 @@ while run:
                     coords = boxes[i][1]
                     clicked[coords[1]][coords[0]] *= -1  # invert the thing when selected if -1 => 1 and vice versa
 
-    beat_length = (fps*60) // bpm
+    beat_length = (fps * 60) // bpm
 
-
-
+    if playing:
+        if active_length < beat_length:
+            active_length += 1
+        else:
+            active_length = 0
+            if active_beat < beats - 1:
+                active_beat += 1
+                beat_changed = True
+            else:
+                active_beat = 0
+                beat_changed = True
 
     pygame.display.flip()
 
